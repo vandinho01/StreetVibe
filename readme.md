3f79ada0269976914cf3b9bb5f1611bbeb6cbb0d
Evandro Menschaid <evandro.menschaid@gmail.com>
2024-03-27 22:25:40 -0300

    street vibe

---
+++ readme.md
@@ -0,0 +1,7 @@
+//COLORS//
+#1E1F20
+#2A4737
+#333333
+
+//FONTES//
+"Poppins", sans-serif
---
+++ script.js
@@ -0,0 +1,11 @@
+let contador = 1;
+
+setInterval(function(){
+  document.getElementById('slide' + contador).checked = true;
+  contador++;
+
+  if(contador > 4){
+    contador = 1;
+  }
+
+}, 4000)
